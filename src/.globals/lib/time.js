b8436a7668cb6e266e2ef3b889ec0f2c75b8af59
Noah Hall <noah.hall@dictionary.com>
2016-12-02 18:45:55 -0800

    update global libraries

--- src/.globals/lib/time.js
+++ src/.globals/lib/time.js
@@ -1,22 +1,41 @@
 import Moment from 'moment';
 require("moment-duration-format");
 
-/**
- * formats and returns time in format required by bart API
- * @method getBartTime
- * @param  {[type]}    time [description]
- * @return {[type]}    [description]
- */
-export const getBartTime = (time) =>
-  time && Moment(time.trim()).format('MM/DD/YYYY h:mm+a').trim();
-export const getTodaysDate = () => Moment().format("YYYY-MM-DD");
-export const getRightNowTime = () => Moment().format("HH:mm");
-export const getDateTimeLocalFormat = () => 'YYYY-MM-DDThh:mm';
-export const getDateFormat = () => 'YYYY-MM-DD';
-export const getTimeFormat = () => 'HH:mm';
-export const getDuration = ({ startTime, startDate, endTime, endDate }) => {
-  const duration = Math.abs(Moment(`${endDate} ${endTime}`, 'MM/DD/YYYY h:mm+a') - Moment(`${startDate} ${startTime}`, 'MM/DD/YYYY h:mm+a'));
+const time = {
+  /**
+   * formats and returns time in format required by bart API
+   * @method getBartTime
+   * @param  {[type]}    time [description]
+   * @return {[type]}    [description]
+   */
+  getBartTime (time) {
+    return time && Moment(time.trim()).format('MM/DD/YYYY h:mm+a').trim();
+  },
 
-  return Moment.duration(duration).format("h [hrs], m [min]");
-};
-export const moment = Moment;
+  getTodaysDate () {
+    return Moment().format("YYYY-MM-DD");
+  },
+
+  getRightNowTime () {
+    return Moment().format("HH:mm");
+  },
+
+  getDateTimeLocalFormat () {
+    return 'YYYY-MM-DDThh:mm';
+  },
+
+  getDateFormat () {
+    return 'YYYY-MM-DD';
+  },
+
+  getTimeFormat () {
+    return 'HH:mm';
+  },
+  getDuration ({ startTime, startDate, endTime, endDate }) {
+    const duration = Math.abs(Moment(`${endDate} ${endTime}`, 'MM/DD/YYYY h:mm+a') - Moment(`${startDate} ${startTime}`, 'MM/DD/YYYY h:mm+a'));
+
+    return Moment.duration(duration).format("h [hrs], m [min]");
+  },
+}
+
+export default time;
